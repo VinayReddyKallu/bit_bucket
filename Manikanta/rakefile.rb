ebf21586ff23244efe0f3c50971c2db8c3af1dbd
Manikanta <mk185388@ncr.com>
2020-05-11 22:31:19 +0530

    ps execution way again

--- Manikanta/rakefile.rb
+++ Manikanta/rakefile.rb
@@ -1,22 +1,21 @@
                 
-task :default => [:hello,:powershell,:thanks]
-
-
-desc 'Greets with hello message'
-  task :hello do
-    puts "HELLO USERS!"
-    puts ""
-  end
+task :default do
+  puts "Welcome User.. You are in default task"
+  puts ""
+  RAKE::Task['powershell'].invoke
+end
 
 desc 'Greets with thank you message'
-  task :thanks => [:hello,:powershell] do
+  task :thanks do
     puts "THANK YOU FOR USING RAKE!!"
     puts ""
   end
 
 desc "Calls the normal powershell script"
-  task :powershell => [:hello] do
+  task :powershell do
     sh 'powershell.exe Manikanta/ps_script3.ps1', verbose: false
+    puts ""
+    RAKE::Task['thanks'].invoke
   end 
 
 
