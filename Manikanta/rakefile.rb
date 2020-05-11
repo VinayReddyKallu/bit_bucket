b5f280abaa855d7513d88004c85f533ac0ea1bf5
Manikanta <mk185388@ncr.com>
2020-05-11 22:49:47 +0530

    ps execution way again

--- Manikanta/rakefile.rb
+++ Manikanta/rakefile.rb
@@ -25,7 +25,6 @@ end
   desc 'Greets with thank you message'
   task :thanks => [:main] do
     puts "THANK YOU FOR USING RAKE!!"
-    puts ""
   end
 #end
 
@@ -51,9 +50,7 @@ end
 
   desc "Calls the normal powershell script"
   task :powershell do
-    puts ""
     sh 'powershell.exe Manikanta/ps_script3.ps1', verbose: false
-    puts ""
   end 
 #end
 
